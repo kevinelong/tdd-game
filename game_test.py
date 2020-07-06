6ae6904840fd351ad5195960fedf09541b718665
kevinelong <kevinelong@gmail.com>
2020-07-06 06:06:46 -0700

    refactor files

--- game_test.py
+++ game_test.py
@@ -1,23 +1,23 @@
 """
-Requirements:
-SETUP:
-    Show Map of specified width and height, default to 9x9.
+Show Map of specified width and height, default to 9x9.
     Show Player center of the Map.
     Place and show a treasure on the map in random position,
      that is not on the player's position.
 
-PLAY:
-    Ask user to if the they want to:
+Ask user to if the they want to:
         A - Rotate-Left
         D - Rotate-Right
         W - Move-Straight
         S - Move-Backward
+
     Rotate or move player Move player in specified direction.
     Reject moves that would go off the game board.
+
     if player and treasure collide then:
         Add treasure's value to score.
         Remove treasure from map.
         Add a new one in an empty position.
+
 """
 from game import Game
 
